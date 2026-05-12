5badf8a6d5b7d34bba3e3d0bfae7221b1ee88a25
Lucas roque <lucasroque0802@gmail.com>
2026-05-12 19:44:53 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿﻿Console.WriteLine("FLUXOGRAMA DA ENGENHARIA");
+
+Console.Write("O objeto está se mexendo (S/N)? ");
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+string mensagem;
+
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    {
+        mensagem = "perfeito!";
+    }
+    else
+    {
+        mensagem = "Use WD-40.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "Perfeito!";
+    }
+    else
+    {
+        mensagem = "Use Silver Tape.";
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(mensagem);
